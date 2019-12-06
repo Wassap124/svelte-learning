8021d57edf8f1228de4c94b4da46a8a99a65e2a4
Wassap124 <17432424+Wassap124@users.noreply.github.com>
2019-12-06 20:40:46 +0200

    ok this is pretty neat

--- src/counter/counter.svelte
+++ src/counter/counter.svelte
@@ -1,17 +1,19 @@
 <script>
   let count = 0;
-  $: squaredCount = count**2;
+  $: squaredCount = count ** 2;
 
   function handleClick() {
     count += 1;
   }
 
-  function handleDoubleClick() {
-      count = 0;
+  // This will trigger every time count changes. i.e - we could write
+  // The condition like this instead of checking it in the `handleClick` function
+  $: if (count > 10) {
+    count = 0;
   }
 </script>
 
-<button on:click={handleClick} on:dblclick|preventDefault={handleDoubleClick}>
+<button on:click={handleClick}>
   Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
 <p>The times that the buttons was clicked - squared - is: {squaredCount}</p>
