e681ea4cc733ff9cd2f42a052a713b3a40647972
Wassap124 <17432424+Wassap124@users.noreply.github.com>
2019-12-06 18:57:17 +0200

    shorthand attr

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   let name = "Alon";
+  let src = "image.gif"
 </script>
 
 <h1>Hello {name.toLowerCase()}!</h1>
-<img src={"image.gif"} alt="You have been rick rolled by {name}"/>
+<img {src} alt="You have been rick rolled by {name}"/>
