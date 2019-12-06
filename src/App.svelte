6254369719d4772db84c883edf1431f8f8c597c4
Wassap124 <17432424+Wassap124@users.noreply.github.com>
2019-12-06 19:21:47 +0200

    using components

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,19 @@
 <script>
-  let name = "Alon";
-  let src = "image.gif"
+  const name = "Alon";
+  const src = "image.gif";
+  import NestedParagraph from "../src/nested-paragraph/nested-paragraph.svelte";
 </script>
 
-<h1>Hello {name.toLowerCase()}!</h1>
-<img {src} alt="You have been rick rolled by {name}"/>
+<style>
+  /* css goes here */
+  .main {
+    text-align: center;
+    margin: 0;
+  }
+</style>
+
+<div class="main">
+  <h1>Hello {name.toLowerCase()}!</h1>
+  <img {src} alt="You have been rick rolled by {name}" />
+  <NestedParagraph />
+</div>
