440d513b90e4aba4449e74e6278b5b13ecdb5684
Wassap124 <17432424+Wassap124@users.noreply.github.com>
2019-12-06 19:51:29 +0200

    inline html string

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   const name = "Alon";
   const src = "image.gif";
   import NestedParagraph from "../src/nested-paragraph/nested-paragraph.svelte";
+  const boringParagraph = "This paragraph is so <strong>boring!</strong>"
 </script>
 
 <style>
@@ -16,4 +17,5 @@
   <h1>Hello {name.toLowerCase()}!</h1>
   <img {src} alt="You have been rick rolled by {name}" />
   <NestedParagraph />
+  <p>{@html boringParagraph}</p>
 </div>
