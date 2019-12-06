8252b1671af746846d2b15ac5e1e43bfe0d0d6b7
Wassap124 <17432424+Wassap124@users.noreply.github.com>
2019-12-06 20:03:16 +0200

    more events

--- src/counter/counter.svelte
+++ src/counter/counter.svelte
@@ -4,8 +4,12 @@
   function handleClick() {
     count += 1;
   }
+
+  function handleDoubleClick() {
+      count = 0;
+  }
 </script>
 
-<button on:click={handleClick}>
+<button on:click={handleClick} on:dblclick|preventDefault={handleDoubleClick}>
   Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
